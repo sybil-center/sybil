cf63cb37e645bef77475e0c8ee800f248117825c
Pavel <Pave1Pal>
2023-03-11 20:48:37 +0100

    refactor: chage CredentialType from enum to string literal type

--- src/types/base/credential-type.type.ts
+++ src/types/base/credential-type.type.ts
@@ -1,34 +1,13 @@
 /**
  * Provided types of VC
- * Can be converted to Kebab case and turn over by {@link toUrlVCType} and {@link toEnumVCType}
+ * Can be converted to Kebab case and turn over by {@link urlCredentialType} and {@link toCredentialType}
  */
-export enum CredentialType {
-  VerifiableCredential = "VerifiableCredential",
-  EmptyCredential = "EmptyCredential",
-  EthAddressExistsCredential = "EthAddressExistsCredential",
-  EthAccountOwnershipCredential = "EthAccountOwnershipCredential",
-  TwitterAccountOwnershipCredential = "TwitterAccountOwnershipCredential",
-  GitHubAccountOwnershipCredential = "GitHubAccountOwnershipCredential",
-  DiscordAccountOwnershipCredential = "DiscordAccountOwnershipCredential"
-}
+export type CredentialType =
+  | "VerifiableCredential"
+  | "EmptyCredential"
+  | "EthAddressExistsCredential"
+  | "EthAccountOwnershipCredential"
+  | "TwitterAccountOwnershipCredential"
+  | "GitHubAccountOwnershipCredential"
+  | "DiscordAccountOwnershipCredential"
 
-export function toUrlVCType(credentialType: CredentialType): string {
-  return credentialType
-    .toString()
-    .replace(/([a-z0–9])([A-Z])/g, "$1-$2")
-    .toLowerCase();
-}
-
-export function toEnumVCType(credentialTypeUrl: string): CredentialType {
-  const vcTypeStr = credentialTypeUrl.replace(/(^\w|-\w)/g, clearAndUpper);
-  // @ts-ignore
-  const vcTypeEnum = CredentialType[vcTypeStr];
-  if (vcTypeEnum) {
-    return vcTypeEnum;
-  }
-  throw new Error(`can not convert ${credentialTypeUrl} to VCType enum`);
-}
-
-function clearAndUpper(text: string) {
-  return text.replace(/-/, "").toUpperCase();
-}
