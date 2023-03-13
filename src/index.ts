215b33f3ed890dadc907092afd29fc59012ff115
Pavel <Pave1Pal>
2023-03-13 10:57:11 +0100

    chore: http-client from GET challange to POST with body

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 export * from "./sybil.js";
 export * from "./signers/eth-request-signer.js";
 export {
+  Credential,
   TwitterAccountVC,
   DiscordAccountVC,
   EthAccountVC,
   GitHubAccountVC,
   SignFn,
-  SignResult
+  SignResult,
+  ChainAlias
 } from "./types/index.js"
 
